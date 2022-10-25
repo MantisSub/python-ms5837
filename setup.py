9ad961d06976574a27e16134caa4bbfd35a67f9f
Axel Busch <axel@xlvisuals.com>
2022-10-25 16:32:21 +1300

    v1.0.1

--- setup.py
+++ setup.py
@@ -3,9 +3,12 @@
 from setuptools import setup, find_packages
 
 setup(name='python-ms5837',
-      version='1.0',
+      version='1.0.1',
       description='A Python3 library for the Blue Robotics MS5837 Bar30 and Bar02 pressure/temperature sensor',
       author='Blue Robotics',
-      url='https://www.bluerobotics.com',
+      url='https://www.bluerobotics.com/',
+      install_requires=[
+            'smbus2',
+      ],
       packages=find_packages()
       )
